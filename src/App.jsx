53b7f5bb21d4426b0caa68eaa5295250eff623e0
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-05-15 22:47:29 +0100

    form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" name="" id="" placeholder="Feed URL or Feeder" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
