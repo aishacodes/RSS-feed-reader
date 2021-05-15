0461e36a8f05dd5f651902fdee9ad62ea724cc52
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-05-15 22:52:26 +0100

    form- handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [feeds, setFeed] = useState([]);
+
+  const addFeed = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <form>
-        <input type="text" name="" id="" placeholder="Feed URL or Feeder" />
-        <button>Add</button>
+      <form onSubmit={addFeed}>
+        <label>URL</label>
+        <div>
+          <input
+            type="text"
+            name="feed"
+            id=""
+            placeholder="Feed URL or Feeder"
+          />
+          <button>Add</button>
+        </div>
       </form>
+
+      {feeds.map((feed) => (
+        <div>{feed}</div>
+      ))}
     </div>
   );
 }
